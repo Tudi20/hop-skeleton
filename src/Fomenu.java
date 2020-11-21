e9371c9cbbd487df78114907d36bb0b1fe9f318f
Tudi20 <tudorkae@gmail.com>
2020-11-22 00:06:47 +0100

    Add Fomenu class

---
+++ src/Fomenu.java
@@ -0,0 +1,52 @@
+
+public static class Fomenu {
+	
+	private static String aktualisFiok;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Program elindult");
+		betoltoKep();
+	}
+	
+	private static void ujJatek() {
+		
+	}
+	
+	private static void hoskozpont() {
+		
+	}
+	
+	private static void beallitasok() {
+		
+	}
+	
+	private static void kilepes() {
+		System.exit(0);
+	}
+	
+	private static void bejelentkezes() {
+		
+	}
+	
+	private static void regisztracio() {
+		
+	}
+	
+	private static void betoltoKep() {
+		System.out.println("Loading...");
+	}
+	
+	private static void fiokMentes() {
+		
+	}
+	
+	public static String getAktualisFiok() {
+		return aktualisFiok;
+	}
+	
+	public static void setAktualisFiok(String _aktualisFiok) {
+		aktualisFiok = _aktualisFiok;
+	}
+
+}
