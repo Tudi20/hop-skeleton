3e721fc94e41d4c464eeaf72180bea427b063fdb
Tudi20 <tudorkae@gmail.com>
2020-11-22 17:09:18 +0100

    Add classes SzobaLetrehozas and Palya

---
+++ src/SzobaLetrehozas.java
@@ -0,0 +1,42 @@
+import java.util.List;
+
+public class SzobaLetrehozas {
+	private String jelszo;
+	private int letszam;
+	private List<Palya> palyaLista;
+	private int palyaAzonosito;
+	private int szobaAzonosito;
+	
+	public static void jatek() {
+		
+	}
+	
+	public void palyaValasztas() {
+		
+	}
+	
+	public void jelszo() {
+		
+	}
+	
+	public void jatekosKezeles() {
+		
+	}
+	
+	public int getLetszam( ) {
+		return letszam;
+	}
+	
+	public String getJelszo( ) {
+		return jelszo;
+	}
+	
+	public void szobaElhagyas() {
+		
+	}
+	
+	public int getPalyaAzonosito( ) {
+		return palyaAzonosito;
+	}
+
+}
