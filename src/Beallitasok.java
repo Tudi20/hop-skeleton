abc7c18f0815e359b9303122ab0b02d532865f55
Tudi20 <tudorkae@gmail.com>
2020-11-22 18:50:00 +0100

    Add class Beallitasok

---
+++ src/Beallitasok.java
@@ -0,0 +1,29 @@
+
+public class Beallitasok {
+	private int hangero;
+	private boolean rezgomod;
+
+	public void hangeroModosit() {
+		
+	}
+	
+	public void rezges() {
+		
+	}
+	
+	public void adomany() {
+		
+	}
+	
+	public void sugo() {
+		
+	}
+	
+	public void fiokvaltas() {
+		
+	}
+	
+	public void beallitasokBezar() {
+		
+	}
+}
