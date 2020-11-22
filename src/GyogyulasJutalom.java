d9d4689d9be471a5f4ef8b7f392dafed3a046ca1
Tudi20 <tudorkae@gmail.com>
2020-11-22 18:38:54 +0100

    Add class GyogyulasJutalom

---
+++ src/GyogyulasJutalom.java
@@ -0,0 +1,16 @@
+
+public class GyogyulasJutalom {
+	public void hirdetes() {
+		
+	}
+	
+	public void tovabblep() {
+		
+	}
+	public void kiirItalszam() {
+		
+	}
+	public void eletFeltoltes() {
+		
+	}
+}
