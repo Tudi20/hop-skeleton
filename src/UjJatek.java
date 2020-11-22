3e2aeae53bc9ce2d4560da0a50aa53230c51cee3
Tudi20 <tudorkae@gmail.com>
2020-11-22 16:57:59 +0100

    Add UjJatek class

---
+++ src/UjJatek.java
@@ -0,0 +1,20 @@
+
+public class UjJatek {
+	private static String szobanev;
+	
+	public static void szobaLetrehozas() {
+		
+	}
+	
+	public static void jatekKereses() {
+		
+	}
+	
+	public static void vasarlas() {
+		
+	}
+	
+	public static String getSzobanev() {
+		return szobanev;
+	}
+}
