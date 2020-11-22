59c4cfdac18a0fe2b43a9ebc9787f85d32a57201
Tudi20 <tudorkae@gmail.com>
2020-11-22 18:31:17 +0100

    Add class Jatek

---
+++ src/Jatek.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class Jatek {
+	private int csapatHelyzet;
+	private List<Jatekos> jatekosLista;
+	
+	public void valasztas() {
+		
+	}
+	
+	public void pauseMenu() {
+		
+	}
+	
+	public void jatekVege() {
+		
+	}
+	
+	public int getCsapatHelyzet() {
+		return csapatHelyzet;
+	}
+	
+	public void mezo() {
+		
+	}
+}
