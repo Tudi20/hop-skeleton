da1c6848ca0c802b135f5c9feff6f1411f4cb9ba
Tudi20 <tudorkae@gmail.com>
2020-11-22 17:35:38 +0100

    Add class Vasarlas

---
+++ src/Vasarlas.java
@@ -0,0 +1,17 @@
+
+public class Vasarlas {
+	private int jogosultsagSzint;
+	
+	public void fizetes() {
+		
+	}
+	
+	public int getJogosultsagSzint( ) {
+		return jogosultsagSzint;
+	}
+	
+	public void jogusultsagModositas() {
+		
+	}
+
+}
