020b311cf4b03f63845d05ceb5cf909985f57518
Tudi20 <tudorkae@gmail.com>
2020-11-22 18:53:25 +0100

    Add class Adomany

---
+++ src/Adomany.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class Adomany {
+	private String alapitvanyNev;
+	private List<String> alapitvanyLista;
+	
+	public String getAlapitvanyNev() {
+		return alapitvanyNev;
+	}
+	
+	public void alpitvanyKivalaszt() {
+		
+	}
+	
+	public void tamogatottOssezg() {
+		
+	}
+
+}
