02a02d6c2b4c2e6b3c99da73823710d5a1e8162c
Tudi20 <tudorkae@gmail.com>
2020-11-22 17:13:19 +0100

    Add class JatekKereses

---
+++ src/JatekKereses.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class JatekKereses {
+	private List<SzobaLetrehozas> szobaLista;
+	
+	public void belepes( ) {
+		
+	}
+	
+	public void jelszoEllenorzes() {
+		
+	}
+	
+	public void szures() {
+		
+	}
+	
+	public void kereses( ) {
+		
+	}
+}
